7c8d5f2a9a8921d5a5c189456ef4f448f81318ed
Yordan2000 <yordanviafara9@gmail.com>
2024-08-29 09:44:06 -0500

    error de CORS

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import router from "./router";
 import db from "./config/db";
-import swaggerUi from 'swagger-ui-express'
-import swaggerSpec from './config/swagger'
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./config/swagger";
 import colors from "colors";
-import cors , { CorsOptions } from "cors";
-import morgan from "morgan"
+import cors, { CorsOptions } from "cors";
+import morgan from "morgan";
 //conexion a base de datos
 async function connectDB() {
   await db.authenticate();
@@ -19,26 +19,25 @@ connectDB();
 const server = express();
 
 //permitir conexion del frontend a backend con cors
-const corsOptions : CorsOptions = {
-
-  origin(origin,callback){
-    if (origin === `${process.env.FRONTEND_URL}`) {
-      callback(null, true)
-    }else{
-      callback(new Error('Error de CRORS'))
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de CORS"));
     }
-  }
-}
-server.use(cors(corsOptions))
+  },
+};
+server.use(cors(corsOptions));
 
 // leer datos DE FORMULARIO
 server.use(express.json());
 
-server.use(morgan('dev'))
+server.use(morgan("dev"));
 
 server.use("/api/products", router);
 
 // documentacion
-server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec) )
+server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default server;
